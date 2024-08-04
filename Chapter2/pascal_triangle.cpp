d4c43c091d41f3ec5fd316383eec7b1a18d03a22
NikBomb <nicola.bombace@gmail.com>
2024-08-04 11:07:07 +0100

    Include format library

--- Chapter2/pascal_triangle.cpp
+++ Chapter2/pascal_triangle.cpp
@@ -2,6 +2,24 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
+#define FMT_HEADER_ONLY
+#include <fmt/core.h>
+
+void show_vectors(std::ostream& s, const std::vector<std::vector<int>>& v) {
+    size_t final_row_size = v.back().size();
+    std::string spaces(final_row_size * 3, ' ');
+    for (const auto& row : v) {
+        s << spaces;
+        if (spaces.size() > 3) {
+            spaces.resize(spaces.size() - 3);
+        }
+        for (const auto& data : row) {
+            s << fmt::format("{: ^{}}", data, 6);
+        }
+        s << '\n';
+    }
+
+}
 
 std::vector<int> get_next_row(const std::vector<int> &last_row)
 {
@@ -11,7 +29,6 @@ std::vector<int> get_next_row(const std::vector<int> &last_row)
     {
         return next_row;
     }
-
     for (size_t idx = 0; idx + 1 <= last_row.size(); ++idx)
     {
         next_row.emplace_back(last_row[idx] + last_row[idx + 1]);
@@ -43,5 +60,6 @@ std::ostream &operator<<(std::ostream &s, const std::vector<std::vector<T>> &tri
 int main()
 {
     auto triangle = generate_triangle(16);
-    std::cout << triangle;
+    //std::cout << triangle;
+    show_vectors(std::cout, triangle);   
 }
